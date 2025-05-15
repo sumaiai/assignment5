769de5424e16b092dfa0a80015b9e6d929c5e1af
sumaiai <sumaiaislam568@gmail.com>
2025-05-15 19:46:52 -0400

    added second page

--- script/script.js
+++ script/script.js
@@ -16,8 +16,143 @@ document.getElementById("completed")
         const d = Date();
         const cardHead = document.getElementById("cardInfo").innerText;
         document.getElementById("update").innerText = "You have completed the task " + cardHead + " on " + d;
+
         document.getElementById("completed").style.display = "none";
         document.getElementById("new").style.display = "block";
     })
 
+
+
 document.getElementById("new").style.display = "none";
+
+
+//second btn
+
+document.getElementById("completed2")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        alert("board updated successfully");
+        const taskNum = document.getElementById("task").innerText;
+        const taskNum2 = parseInt(taskNum);
+        const displayNum = taskNum2 - 1;
+        document.getElementById("task").innerText = displayNum;
+
+        const nav1 = document.getElementById("checkNum").innerText;
+        const nav2 = parseInt(nav1);
+        const displayNav = nav2 + 1;
+        document.getElementById("checkNum").innerText = displayNav;
+
+        // const side = document.getElementById("update").innerText;
+        const d = Date();
+        const cardHead = document.getElementById("cardInfo2").innerText;
+        document.getElementById("update").innerText = "You have completed the task " + cardHead + " on " + d;
+        document.getElementById("completed2").style.display = "none";
+        document.getElementById("new2").style.display = "block";
+    })
+
+document.getElementById("new2").style.display = "none";
+
+//third btn
+
+document.getElementById("completed3")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        alert("board updated successfully");
+        const taskNum = document.getElementById("task").innerText;
+        const taskNum2 = parseInt(taskNum);
+        const displayNum = taskNum2 - 1;
+        document.getElementById("task").innerText = displayNum;
+
+        const nav1 = document.getElementById("checkNum").innerText;
+        const nav2 = parseInt(nav1);
+        const displayNav = nav2 + 1;
+        document.getElementById("checkNum").innerText = displayNav;
+
+        // const side = document.getElementById("update").innerText;
+        const d = Date();
+        const cardHead = document.getElementById("cardInfo3").innerText;
+        document.getElementById("update").innerText = "You have completed the task " + cardHead + " on " + d;
+        document.getElementById("completed3").style.display = "none";
+        document.getElementById("new3").style.display = "block";
+    })
+
+document.getElementById("new3").style.display = "none";
+
+//four btn
+
+document.getElementById("completed4")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        alert("board updated successfully");
+        const taskNum = document.getElementById("task").innerText;
+        const taskNum2 = parseInt(taskNum);
+        const displayNum = taskNum2 - 1;
+        document.getElementById("task").innerText = displayNum;
+
+        const nav1 = document.getElementById("checkNum").innerText;
+        const nav2 = parseInt(nav1);
+        const displayNav = nav2 + 1;
+        document.getElementById("checkNum").innerText = displayNav;
+
+        // const side = document.getElementById("update").innerText;
+        const d = Date();
+        const cardHead = document.getElementById("cardInfo4").innerText;
+        document.getElementById("update").innerText = "You have completed the task " + cardHead + " on " + d;
+        document.getElementById("completed4").style.display = "none";
+        document.getElementById("new4").style.display = "block";
+    })
+
+document.getElementById("new4").style.display = "none";
+
+//5th btn
+
+document.getElementById("completed5")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        alert("board updated successfully");
+        const taskNum = document.getElementById("task").innerText;
+        const taskNum2 = parseInt(taskNum);
+        const displayNum = taskNum2 - 1;
+        document.getElementById("task").innerText = displayNum;
+
+        const nav1 = document.getElementById("checkNum").innerText;
+        const nav2 = parseInt(nav1);
+        const displayNav = nav2 + 1;
+        document.getElementById("checkNum").innerText = displayNav;
+
+        // const side = document.getElementById("update").innerText;
+        const d = Date();
+        const cardHead = document.getElementById("cardInfo5").innerText;
+        document.getElementById("update").innerText = "You have completed the task " + cardHead + " on " + d;
+        document.getElementById("completed5").style.display = "none";
+        document.getElementById("new5").style.display = "block";
+    })
+
+document.getElementById("new5").style.display = "none";
+
+
+//6th btn
+
+document.getElementById("completed6")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        alert("board updated successfully");
+        const taskNum = document.getElementById("task").innerText;
+        const taskNum2 = parseInt(taskNum);
+        const displayNum = taskNum2 - 1;
+        document.getElementById("task").innerText = displayNum;
+
+        const nav1 = document.getElementById("checkNum").innerText;
+        const nav2 = parseInt(nav1);
+        const displayNav = nav2 + 1;
+        document.getElementById("checkNum").innerText = displayNav;
+
+        // const side = document.getElementById("update").innerText;
+        const d = Date();
+        const cardHead = document.getElementById("cardInfo6").innerText;
+        document.getElementById("update").innerText = "You have completed the task " + cardHead + " on " + d;
+        document.getElementById("completed6").style.display = "none";
+        document.getElementById("new6").style.display = "block";
+    })
+
+document.getElementById("new6").style.display = "none";
